f4d7337ed90eac58bb0565a7ce79f82d2a4a19fe
Atar MP <suroor.atar@gmail.com>
2022-12-14 01:21:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import http from "http";
+import { ApolloServer, gql } from "apollo-server-express";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+const app = express();
+const PORT = 4000;
+
+const server = new ApolloServer({
+  typeDefs: gql(typeDefs),
+  resolvers,
+  subscriptions: {
+    onConnect: () => console.log("Connected to websocket"),
+  },
+  tracing: true,
+});
+
+server.applyMiddleware({ app });
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen({ port: PORT }, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`
+  );
+});
